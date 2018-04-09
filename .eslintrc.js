a87b3faa8f6d62f05268110b10966d1d3ca2e292
王玲波 <454949876@qq.com>
2018-04-09 10:55:14 +0800

    代码格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 // https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -11,15 +10,11 @@ module.exports = {
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
-  // required to lint *.vue files
   plugins: [
     'html'
   ],
-  // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
     'generator-star-spacing': 0,
     'camelcase': [0, {
       'properties': 'always'
@@ -38,7 +33,6 @@ module.exports = {
     'no-multiple-empty-lines': [2, {
       'max': 1
     }],
-    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
