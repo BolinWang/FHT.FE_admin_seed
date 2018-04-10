ac1845fdeb7971859e88727b2e70f9026cfbc28b
王玲波 <454949876@qq.com>
2018-04-10 11:44:21 +0800

    adios封装完成（防止重复提交、捕获接口报错、鉴权、重定向、promise包装暴露fetch方法等）

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -6,7 +6,8 @@ import { getSessionId } from '@/utils/auth'
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.BASE_API,
-  interceptors: true, // 是否开启response拦截器
+  interceptors: true, // 是否开启response拦截器 默认true
+  noAssign: false, // 请求体是否带defaultConfig 默认false
   // timeout: 5000 // 请求超时时间
 })
 
@@ -14,38 +15,42 @@ const service = axios.create({
 let cancelPromise, requestList = {}
 const CancelToken = axios.CancelToken
 
+/* 默认请求参数 */
+const defaultConfig = {
+  version: "1.0",
+  timestamp: new Date().getTime(),
+  reqId: "0010C2379272774D6EC087B917CE2A71438DEF90",
+  sign: "8F4C4A8E9D850EDD9692DE38723D0543"
+}
+
 // request拦截器
 service.interceptors.request.use(config => {
-  //发起请求时，取消掉当前正在进行的相同请求
-  const requestUrlAndMethod = config.url + config.data.method
+  /* 发起请求时，取消掉当前正在进行的相同请求 */
+  const dataMethod =  config.method.toUpperCase() === 'POST' ? config.data.method : config.params.method
+  const requestUrlAndMethod = config.url + dataMethod
   if (requestList[requestUrlAndMethod]) {
     requestList[requestUrlAndMethod]('操作取消')
     requestList[requestUrlAndMethod] = cancelPromise
   } else {
     requestList[requestUrlAndMethod] = cancelPromise
   }
-  const defaultConfig = {
-    version: "1.0",
-    timestamp: new Date().getTime(),
-    reqId: "0010C2379272774D6EC087B917CE2A71438DEF90",
-    sign: "8F4C4A8E9D850EDD9692DE38723D0543"
-  }
+  /* post请求 */
   if (config.method.toUpperCase() == 'POST') {
     if (store.getters.sessionId) {
-      config.data['sessionId'] = getSessionId();
+      config.data['sessionId'] = getSessionId()
     }
     if (!config.noAssign) {
-      config.data = Object.assign(config.data, defaultConfig);
+      config.data = Object.assign(config.data, defaultConfig)
     }
   } else {
     if (store.getters.sessionId) {
-      config.params['sessionId'] = getSessionId();
+      config.params['sessionId'] = getSessionId()
     }
     if (!config.noAssign) {
-      config.params = Object.assign(config.params, defaultConfig);
+      config.params = Object.assign(config.params, defaultConfig)
     }
   }
-  return config;
+  return config
 }, error => {
   console.log('【REQUEST】' + error)
   Promise.reject(error)
@@ -63,7 +68,7 @@ service.interceptors.response.use(
       type: 'error',
       duration: 5000
     })
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
 )
 
@@ -73,11 +78,9 @@ const responseMehod = (response, resolve, reject) => {
     return resolve(res)
   }
   if (res.code == 1011) {
-    if (res.message == '无数据') {
-      return response.data;
-    } else if (res.message == '数据不存在') {
+    if (res.message == '数据不存在') {
       store.dispatch('FedLogOut').then(() => {
-        location.reload(); // 为了重新实例化vue-router对象 避免bug
+        location.reload() // 为了重新实例化vue-router对象 避免bug
       })
     }
   }
@@ -85,16 +88,16 @@ const responseMehod = (response, resolve, reject) => {
     message: res.message || '未知错误，请联系管理员',
     type: 'error',
     duration: 5 * 1000
-  });
+  })
   // sessionId 失效
   if (res.code == 1016) {
-    MessageBox.confirm(res.message + '，请重新登录', '提示', {
+    MessageBox.confirm(`${res.message}，请重新登录`, '提示', {
       confirmButtonText: '重新登录',
       cancelButtonText: '取消',
       type: 'warning'
     }).then(() => {
       store.dispatch('FedLogOut').then(() => {
-        location.reload(); // 为了重新实例化vue-router对象 避免bug
+        location.reload() // 为了重新实例化vue-router对象 避免bug
       })
     })
   }
@@ -109,7 +112,7 @@ const judgeMethod = (url, params, method = 'post') => {
       cancelPromise = c
     })
   }
-  if (method === 'post') {
+  if (method.toUpperCase() === 'POST') {
     requestBody.data = params
   } else {
     requestBody.params = params
