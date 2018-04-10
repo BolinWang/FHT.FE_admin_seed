0de0484bdce4704b48064f4a27f698f2698f17f7
王玲波 <454949876@qq.com>
2018-04-10 14:57:31 +0800

    response拦截器，重复请求的error不提示

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -3,16 +3,8 @@ import { Message, MessageBox } from 'element-ui'
 import store from '../store'
 import { getSessionId } from '@/utils/auth'
 
-// 创建axios实例
-const service = axios.create({
-  baseURL: process.env.BASE_API,
-  interceptors: true, // 是否开启response拦截器 默认true
-  noAssign: false, // 请求体是否带defaultConfig 默认false
-  // timeout: 5000 // 请求超时时间
-})
-
 /* 防止重复提交，利用axios的cancelToken */
-let cancelPromise, requestList = {}
+let cancelPromise, requestPath = {}
 const CancelToken = axios.CancelToken
 
 /* 默认请求参数 */
@@ -23,16 +15,24 @@ const defaultConfig = {
   sign: "8F4C4A8E9D850EDD9692DE38723D0543"
 }
 
-// request拦截器
+/* 创建axios实例 */
+const service = axios.create({
+  baseURL: process.env.BASE_API,
+  interceptors: true, // 是否开启response拦截器 默认true
+  noAssign: false, // 请求体是否带defaultConfig 默认false
+  // timeout: 5000 // 请求超时时间
+})
+
+/* request拦截器 */
 service.interceptors.request.use(config => {
   /* 发起请求时，取消掉当前正在进行的相同请求 */
   const dataMethod =  config.method.toUpperCase() === 'POST' ? config.data.method : config.params.method
-  const requestUrlAndMethod = config.url + dataMethod
-  if (requestList[requestUrlAndMethod]) {
-    requestList[requestUrlAndMethod]('操作取消')
-    requestList[requestUrlAndMethod] = cancelPromise
+  const requestUrlAndMethod = (config.url.endsWith('/') ? config.url : `${config.url}/`) + dataMethod
+  if (requestPath[requestUrlAndMethod]) {
+    requestPath[requestUrlAndMethod]('取消重复请求')
+    requestPath[requestUrlAndMethod] = cancelPromise
   } else {
-    requestList[requestUrlAndMethod] = cancelPromise
+    requestPath[requestUrlAndMethod] = cancelPromise
   }
   /* post请求 */
   if (config.method.toUpperCase() == 'POST') {
@@ -56,13 +56,16 @@ service.interceptors.request.use(config => {
   Promise.reject(error)
 })
 
-// respone拦截器
+/* respone拦截器 */
 service.interceptors.response.use(
   response => {
     return response
   },
   error => {
     console.log('【response】' + error)
+    if (error.message === '取消重复请求') {
+      return Promise.reject(error)
+    }
     Message({
       message: '连接服务器失败',
       type: 'error',
@@ -72,17 +75,21 @@ service.interceptors.response.use(
   }
 )
 
+/* 调用退出系统 */
+const logOutMethod = () => {
+  store.dispatch('FedLogOut').then(() => {
+    location.reload() // 为了重新实例化vue-router对象 避免bug
+  })
+}
+
+/* axios请求体包装 */
 const responseMehod = (response, resolve, reject) => {
   const res = response.data
   if (!response.config.interceptors || res.code == 0) {
     return resolve(res)
   }
-  if (res.code == 1011) {
-    if (res.message == '数据不存在') {
-      store.dispatch('FedLogOut').then(() => {
-        location.reload() // 为了重新实例化vue-router对象 避免bug
-      })
-    }
+  if (res.code == 1011 && res.message == '数据不存在') {
+    logOutMethod()
   }
   Message({
     message: res.message || '未知错误，请联系管理员',
@@ -96,9 +103,7 @@ const responseMehod = (response, resolve, reject) => {
       cancelButtonText: '取消',
       type: 'warning'
     }).then(() => {
-      store.dispatch('FedLogOut').then(() => {
-        location.reload() // 为了重新实例化vue-router对象 避免bug
-      })
+      logOutMethod()
     })
   }
   return reject("error")
