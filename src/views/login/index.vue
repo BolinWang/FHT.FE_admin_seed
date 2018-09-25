d6ec1fd104f3b08dbc62cca7ae75e1386ed218dc
王玲波 <454949876@qq.com>
2018-09-25 18:56:05 +0800

    ### *柏林*  登录页V2.0

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 17:22:52
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-09-17 10:48:32
+ * @Last Modified time: 2018-09-25 18:52:41
  */
 
 <template>
@@ -11,41 +11,47 @@
       ref="loginForm"
       :model="loginForm"
       :rules="loginRules"
-      auto-complete="on"
       label-position="left"
       label-width="0px"
       class="card-box login-form">
-      <h3 class="title">FT_admin-seed</h3>
-      <el-form-item prop="mobile">
+      <h3 class="title">欢迎登录</h3>
+      <el-form-item
+        class="login_item"
+        prop="mobile">
         <el-input
           v-model="loginForm.mobile"
           name="mobile"
           type="text"
-          auto-complete="on"
-          placeholder="请输入用户名">
+          autocomplete="off"
+          clearable
+          placeholder="请输入账号">
           <i
             slot="prefix"
-            class="iconfont icon-user" />
+            class="iconfont icon-user-fill" />
         </el-input>
       </el-form-item>
-      <el-form-item prop="password">
+      <el-form-item
+        class="login_item"
+        prop="password">
         <el-input
           v-model="loginForm.password"
           name="password"
           type="password"
-          auto-complete="on"
+          clearable
+          autocomplete="off"
           placeholder="请输入密码"
           @keyup.enter.native="handleLogin">
           <i
             slot="prefix"
-            class="iconfont icon-lock" />
+            class="iconfont icon-lock-fill-open" />
         </el-input>
       </el-form-item>
-      <el-form-item>
+      <el-form-item class="login_handle">
         <el-button
           :loading="loading"
           type="primary"
           style="width:100%;"
+          class="btn_login"
           @click.native.prevent="handleLogin">
           登录
         </el-button>
@@ -54,21 +60,21 @@
   </div>
 </template>
 <script>
-import { validateMobile } from '@/utils/validate'
+// import { validateMobile } from '@/utils/validate'
 
 export default {
   name: 'Login',
   data () {
-    const validatePhone = (rule, value, callback) => {
-      if (!validateMobile(value.trim())) {
-        callback(new Error('请输入正确的手机号'))
+    const validateAccount = (rule, value, callback) => {
+      if (!value.trim()) {
+        callback(new Error('请输入账号'))
       } else {
         callback()
       }
     }
     const validatePass = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('密码不能小于6位'))
+      if (!value) {
+        callback(new Error('请输入密码'))
       } else {
         callback()
       }
@@ -80,7 +86,7 @@ export default {
       },
       loginRules: {
         mobile: [
-          { required: true, trigger: 'blur', validator: validatePhone }
+          { required: true, trigger: 'blur', validator: validateAccount }
         ],
         password: [
           { required: true, trigger: 'blur', validator: validatePass }
@@ -112,70 +118,81 @@ export default {
 @import "src/styles/mixin.scss";
 
 .login-container {
-  @include relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
   height: 100%;
-  background-color: #2d3a4b;
-  background-image: url(../../assets/banner.jpg);
+  background-color:rgba(14, 14, 14, 1);
+  background-image: url(../../assets/bg_login.png);
   background-size: cover;
   background-position: center center;
-  input:-webkit-autofill {
-    box-shadow: 0 0 0px 1000px rgb(133, 133, 133) inset !important;
-    -webkit-box-shadow: 0 0 0px 1000px rgb(133, 133, 133) inset !important;
-    -webkit-text-fill-color: #fff !important;
-  }
+  // input:-webkit-autofill {
+  //   box-shadow: 0 0 0px 1000px rgb(255, 255, 255) inset !important;
+  //   -webkit-box-shadow: 0 0 0px 1000px rgb(255, 255, 255) inset !important;
+  //   -webkit-text-fill-color: #fff !important;
+  // }
   input {
     background: transparent;
     border: 0px;
     -webkit-appearance: none;
     border-radius: 0px;
-    padding: 12px 5px 12px 15px;
-    color: #fff;
-    height: 50px;
-    line-height: 50px;
+    padding: 8px 40px;
+    &.el-input__inner {
+      padding-left: 40px;
+    }
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus,
+    &:-webkit-autofill:active {
+      transition-delay: 999999s;
+      transition: color 999999s ease-out, background-color 999999s ease-out;
+      -webkit-transition-delay: 999999s;
+      -webkit-transition: color 999999s ease-out, background-color 999999s ease-out;
+    }
   }
   .login-form {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    width: 400px;
-    padding: 35px 35px 15px 35px;
-    margin-top: -220px;
-    margin-left: -200px;
-    background-color: rgba(45, 58, 75, 0.4);
-    border-radius: 10px;
-    .el-input {
-      display: inline-block;
-      height: 100%;
-      width: 100%;
+    width: 450px;
+    padding: 50px;
+    background: rgba(255, 255, 255, 0.83);
+    border-radius: 12px;
+    font-size: 16px;
+    color: #333;
+    .login_item {
+      border-bottom: 1px solid #CFCFCF;
+      margin-bottom: 30px;
+      .el-input {
+        display: inline-block;
+        height: 100%;
+        width: 100%;
+        font-size: 16px;
+      }
+      .el-form-item__error {
+        padding-top: 10px;
+      }
+    }
+    .login_handle {
+      margin-bottom: 0;
+      padding-top: 20px;
+      .btn_login {
+        height: 50px;
+        font-size: 18px;
+        background: #4680FF;
+        border-radius: 8px;
+        border-color: #4680ff;
+      }
     }
     .title {
-      font-size: 26px;
-      font-weight: 400;
-      color: #fff;
-      margin: 0px auto 40px auto;
+      font-size: 28px;
+      color: #333;
       text-align: center;
-      font-weight: bold;
+      font-weight: normal;
     }
     .iconfont {
       width: 25px;
-      height: 50px;
-      line-height: 50px;
-      font-size: 16px;
+      font-size: 22px;
+      color: #999;
     }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.3);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-    margin-bottom: 30px;
-  }
-
-  .el-form-item__error {
-    color: #ff0;
-    font-size: 13px;
-    line-height: 24px;
   }
 }
 
