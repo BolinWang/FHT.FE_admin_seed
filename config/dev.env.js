581f1b40de1fa8a31c2fb62944ee6d7a30104e9d
王玲波 <454949876@qq.com>
2018-10-12 13:59:56 +0800

    ### *柏林* 新增dev环境打包

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,11 +2,11 @@
  * @Author: FT.FE.Bolin
  * @Date: 2018-04-11 16:29:26
  * @Last Modified by: FT.FE.Bolin
- * @Last Modified time: 2018-09-20 14:18:17
+ * @Last Modified time: 2018-10-12 13:57:42
  */
 
 module.exports = {
-  NODE_ENV: '"development"',
+  NODE_ENV: '"production"',
   ENV_CONFIG: '"dev"',
   NPM_CONFIG_REPORT: '"false"',
   BASE_API: '"https://dev.mdguanjia.com/example/"',
