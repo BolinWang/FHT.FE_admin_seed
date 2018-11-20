dd7032973c6760e4b441b37445023a7939bdf3a7
王玲波 <454949876@qq.com>
2018-11-20 11:05:57 +0800

    ### *柏林* 上传/下载Excel demoPages

---
+++ src/views/example/downloadExcel.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="components-container">
+    <el-button
+      :loading="downloadLoading"
+      type="primary"
+      size="small"
+      icon="el-icon-download"
+      @click="exportExcel">下载Excel</el-button>
+  </div>
+</template>
+
+<script>
+import { exampleApi } from '@/api/example'
+
+export default {
+  name: 'DownloadExcel',
+  data () {
+    return {
+      downloadLoading: false,
+      colModels: [
+        {prop: 'title', label: '标题'},
+        {prop: 'introduction', label: '简介'},
+        {prop: 'bolin', label: '不导出', noExport: true}
+      ]
+    }
+  },
+  methods: {
+    // 下载Excel
+    exportExcel () {
+      this.downloadLoading = true
+      exampleApi.list({
+        pageNo: 1,
+        pageSize: 999999
+      }).then(response => {
+        const resData = (response.data.content || []).slice()
+        resData.map((item, index) => {
+          item.index = index * 1 + 1
+          item.status = item.statusStr
+        })
+        require.ensure([], () => {
+          const { export_json_to_excel } = require('@/vendor/Export2Excel')
+          const filterCols = this.colModels.filter((item) => {
+            return !item.noExport
+          })
+          const tHeader = ['序号', ...(filterCols.map((item) => item.label))]
+          const filterVal = ['index', ...(filterCols.map((item) => item.prop))]
+          console.log(tHeader, filterVal)
+          const data = this.formatJson(filterVal, resData)
+          export_json_to_excel(tHeader, data, '下载的Excel名', '下载的sheet名')
+          this.downloadLoading = false
+        })
+      }).catch((err) => {
+        console.log(err)
+        this.downloadLoading = false
+      })
+    },
+    // 数据JSON处理
+    formatJson (filterVal, jsonData) {
+      return jsonData.map(v => filterVal.map(j => {
+        return v[j]
+      }))
+    }
+  }
+}
+</script>
---
+++ src/views/example/uploadExcel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="components-container">
+    <upload-excel-component
+      :on-success="handleSuccess"
+      :before-upload="beforeUpload"/>
+    <el-table
+      :data="tableData"
+      border
+      highlight-current-row
+      style="width: 100%;margin-top:20px;">
+      <el-table-column
+        v-for="item of tableHeader"
+        :prop="item"
+        :label="item"
+        :key="item"/>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import UploadExcelComponent from '@/components/UploadExcel'
+
+export default {
+  name: 'UploadExcel',
+  components: { UploadExcelComponent },
+  data () {
+    return {
+      tableData: [],
+      tableHeader: []
+    }
+  },
+  methods: {
+    beforeUpload (file) {
+      const isLimit1M = file.size / 1024 / 1024 < 1
+      if (!isLimit1M) {
+        this.$message.warning('文件大小不能超过1M！')
+        return false
+      }
+      return true
+    },
+    handleSuccess ({ results, header }) {
+      this.tableData = results
+      this.tableHeader = header
+    }
+  }
+}
+</script>
