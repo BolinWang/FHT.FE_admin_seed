65fee59df74c000659630d109c95a5dde45d4011
王玲波 <454949876@qq.com>
2018-11-20 17:17:39 +0800

    ### *柏林* 增加省市区联选demo

---
+++ src/views/example/areaSelect.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="components-container">
+    <div style="width: 300px;">
+      <area-select
+        ref="areaSelect"
+        v-model="areaCode"
+        :level="1"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import areaSelect from '@/components/AreaSelect'
+
+export default {
+  name: 'ExampleAreaSelect',
+  components: {
+    areaSelect
+  },
+  data () {
+    return {
+      areaCode: null
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
